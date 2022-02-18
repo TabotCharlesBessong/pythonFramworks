55fe6ce1b31d7d78e0b2dd3be868e00f7cb08eb0
Charles Bessong Tabot <ebezebeatrice@gmail.com>
2022-02-18 12:33:41 +0100

    innitialising repo

---
+++ Tkinter/add.py
@@ -0,0 +1,6 @@
+
+print("Hello world")
+a = 12
+b =18
+c = a + b 
+print(c)
---
+++ Tkinter/text.py
@@ -0,0 +1,8 @@
+
+print('''
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dolorem accusamus 
+      obcaecati harum maxime nemo illum earum odio corporis aliquam, iusto laudantium 
+      quibusdam reiciendis a vel velit unde ducimus ratione! Dolore id ullam quasi et       modi
+      veniam ab perferendis.
+      ''')
+
