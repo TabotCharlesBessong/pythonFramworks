f8df50f93b768aa8f93c029324cadf00e9cba4f7
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-04-02 16:08:05 +0100

    setting up our first scrapping project

---
+++ scrapy/bookscraper/bookscraper/spiders/bookspider.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["http://books.toscrape.com/"]
+
+    def parse(self, response):
+        pass
