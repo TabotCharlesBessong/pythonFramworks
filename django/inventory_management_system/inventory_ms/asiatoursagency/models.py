4a51f5b552b73ebbf89e0687026ec5052886baff
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-09-02 15:58:47 +0100

    migrating our data

--- django/inventory_management_system/inventory_ms/asiatoursagency/models.py
+++ django/inventory_management_system/inventory_ms/asiatoursagency/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Tour(models.Model):
+  # we need country,destination,number of night, price
+  origin_country = models.CharField(max_length=64)
+  destination_country = models.CharField(max_length=64)
+  number_of_night = models.IntegerField()
+  price = models.IntegerField()
