b43ef4efdb96a0720443bc999eab508d1da1ae7b
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-09-02 17:06:04 +0100

    crud operation on my models

--- django/inventory_management_system/inventory_ms/asiatoursagency/models.py
+++ django/inventory_management_system/inventory_ms/asiatoursagency/models.py
@@ -7,3 +7,7 @@ class Tour(models.Model):
   destination_country = models.CharField(max_length=64)
   number_of_night = models.IntegerField()
   price = models.IntegerField()
+  
+  # this is a string representation of the tours
+  def __str__(self):
+    return (f"ID:{self.id}: From {self.origin_country} - {self.destination_country} for {self.number_of_night} night(s) at ${self.price}")
--- django/inventory_management_system/inventory_ms/asiatoursagency/views.py
+++ django/inventory_management_system/inventory_ms/asiatoursagency/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Tour
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello world\nWelcome to the asia tour agency")
+  tours = Tour.objects.all()
+  context = {'tours':tours}
+  return render(request,"tours/index.html",context)
